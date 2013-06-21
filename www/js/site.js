aeb68ac101dfd1cd608af57b73d540fac50806b2
Doug Neiner <doug@pixelgraphics.us>
2013-06-21 00:35:50 -0400

    Added dialog popover

--- www/js/site.js
+++ www/js/site.js
@@ -1,13 +1,31 @@
 (function ( $ ) {
 
 var $doc = $( document );
+var $body = $( document.body );
+
+$doc.on( "submit", function ( e ) {
+	e.preventDefault();
+});
 
 $doc.on( "click", ".visual-select li", function () {
-	var $el = $( this );
-	var index = $el.index();
+	var $el   = $( this ),
+		index = $el.index(),
+		select;
+
 	$el.siblings().removeClass( "current" );
 	$el.addClass( "current" );
-	$el.closest( ".visual-select" ).prev()[0].selectedIndex = index;
+	select = $el.closest( ".visual-select" ).prev()[0];
+	select.selectedIndex = index;
+});
+
+$doc.on( "click", ".quick-view-overlay, .quick-view-close", function ( e ) {
+	$body.removeClass( "show-quick-view" );
+	e.preventDefault();
+});
+
+$doc.on( "click", ".product", function ( e ) {
+	$body.addClass( "show-quick-view" );
+	e.preventDefault();
 });
 
 
